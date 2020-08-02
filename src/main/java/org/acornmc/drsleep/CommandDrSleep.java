000402d7bcbd6b87fb69ea1d0e76adac7cf7d6c4
DrBot7 <46543473+DrBot7@users.noreply.github.com>
2020-08-02 12:38:19 -0500

    Fix drsleep clear
    Fixes the internal error when more than 1 item was on the nosleep list

--- src/main/java/org/acornmc/drsleep/CommandDrSleep.java
+++ src/main/java/org/acornmc/drsleep/CommandDrSleep.java
@@ -1,13 +1,15 @@
 package org.acornmc.drsleep;
 
 import org.acornmc.drsleep.configuration.ConfigManager;
-import org.bukkit.entity.Player;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+
+import java.util.Iterator;
 import java.util.UUID;
 import java.util.Set;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
 
 public class CommandDrSleep implements CommandExecutor {
     Set<UUID> nosleep;
@@ -24,43 +26,47 @@ public class CommandDrSleep implements CommandExecutor {
             return false;
         }
         if (args[0].equals("reload")) {
-            if (sender.hasPermission("drsleep.reload")) {
-                configManager.reload();
-                sender.sendMessage("§6DrSleep config reloaded.");
+            if (!sender.hasPermission("drsleep.reload")) {
+                sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
                 return true;
             }
-            sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
+            configManager.reload();
+            sender.sendMessage("§6DrSleep config reloaded.");
             return true;
         }
         if (args[0].equals("list")) {
-            if (sender.hasPermission("drsleep.list")) {
-                int count = nosleep.size();
-                String list = configManager.get().getString("NoSleepList").replace("&", "§").replace("%count%", Integer.toString(count));
-                for (UUID uuid : this.nosleep) {
-                    String playername = Bukkit.getPlayer(uuid).getName();
-                    list += " " + playername;
-                }
-                sender.sendMessage(list);
+            if (!sender.hasPermission("drsleep.list")) {
+                sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
                 return true;
             }
-            sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
+            int count = nosleep.size();
+            String list = configManager.get().getString("NoSleepList").replace("&", "§").replace("%count%", Integer.toString(count));
+            for (UUID uuid : this.nosleep) {
+                String playername = Bukkit.getPlayer(uuid).getName();
+                list += " " + playername;
+            }
+            sender.sendMessage(list);
             return true;
         }
         if (args[0].equals("clear")) {
-            if (sender.hasPermission("drsleep.clear")) {
-                int count = nosleep.size();
-                String names = configManager.get().getString("NoSleepClear").replace("&", "§").replace("%count%", Integer.toString(count));
-                for (final UUID uuid : nosleep) {
-                    final Player player = Bukkit.getPlayer(uuid);
-                    if (player != null) {
-                        nosleep.remove(player.getUniqueId());
-                        names += " " + player.getName();
-                    }
-                }
-                sender.sendMessage(names);
+            if (!sender.hasPermission("drsleep.clear")) {
+                sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
                 return true;
             }
-            sender.sendMessage(configManager.get().getString("NoPerms").replace("&", "§"));
+            int count = nosleep.size();
+            String names = configManager.get().getString("NoSleepClear").replace("&", "§").replace("%count%", Integer.toString(count));
+            Iterator<UUID> iterator = nosleep.iterator();
+            while (iterator.hasNext()) {
+                Player player = Bukkit.getPlayer(iterator.next());
+                iterator.remove();
+                String playername = "{null}";
+                if (player != null) {
+                    playername = player.getName();
+                }
+                player.sendMessage(configManager.get().getString("RemovedFromNoSleep").replace("&", "§"));
+                names += " " + playername;
+            }
+            sender.sendMessage(names);
             return true;
         }
         return false;

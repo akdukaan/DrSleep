48a43104a1a37a26742a96e37c2446add61b9662
saboooor <saboor547@gmail.com>
2023-06-21 22:47:59 -0600

    Add smooth sleep

--- src/main/java/org/acornmc/drsleep/EventPlayerBedEnter.java
+++ src/main/java/org/acornmc/drsleep/EventPlayerBedEnter.java
@@ -6,6 +6,8 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+import static org.acornmc.drsleep.DrSleep.plugin;
 
 public class EventPlayerBedEnter implements Listener {
 
@@ -22,17 +24,29 @@ public class EventPlayerBedEnter implements Listener {
             Debug.log(ManagedWorld.managedWorlds.toString());
             return;
         }
-        if (m.preventingSleep.size() > 0) {
+        if (m.preventingSleep.isEmpty()) {
             Lang.send(player, Lang.CANNOT_SKIP);
             return;
         }
-        w.setTime(0L);
+
         if (w.isThundering()) {
             w.setThundering(false);
         }
         if (w.hasStorm()) {
             w.setStorm(false);
         }
+
+        BukkitRunnable runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                w.setTime(w.getTime() + 100);
+                if (w.getTime() < 12542) {
+                    cancel();
+                }
+            }
+        };
+        runnable.runTaskTimer(plugin, 1L, 1L);
+
         String msg = Lang.SKIPPED_NIGHT;
         msg = msg.replace("%PLAYER%", player.getName());
         for (Player p : w.getPlayers()) {
